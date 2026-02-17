c5bc3b20556583f1b59ce800102556c358b2786e
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-17 23:40:28 +0800

    P2368

---
+++ Luogu/P2368/P2368/test.c
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int n;
+
+int main()
+{
+	scanf("%d", &n);
+	if (n < 9)
+	{
+		printf("0\n");
+	}
+	else if (n == 9)
+	{
+		printf("8\n");
+	}
+	else
+	{
+		printf("72");
+		for (int i = 0; i < n - 10; i++)
+		{
+			printf("0");
+		}
+		printf("\n");
+	}
+	return 0;
+}
