b3c54f7d578bcdadf9c2571230cf17c4ce457e74
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-18 17:30:44 +0800

    力扣两数之和

--- LeetCode/1_twoSum/1_twoSum/test.c
+++ LeetCode/1_twoSum/1_twoSum/test.c
@@ -1,1 +1,22 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+	for (int i = 0; i < numsSize; i++)
+	{
+		for (int j = i + 1; j < numsSize; j++)
+		{
+			if (*(nums + j) == target - *(nums + i))
+			{
+				int* ret = (int*)malloc(sizeof(int) * 2);
+				ret[0] = i;
+				ret[1] = j;
+				*returnSize = 2;
+				return ret;
+			}
+		}
+	}
+	*returnSize = 0;
+	return NULL;
+}
