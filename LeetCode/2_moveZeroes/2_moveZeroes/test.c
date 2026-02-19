d57627affa18a129de07390e97e628214dc873c0
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-19 17:05:59 +0800

    力扣移动零

---
+++ LeetCode/2_moveZeroes/2_moveZeroes/test.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+void moveZeroes(int* nums, int numsSize) {
+	int i = 0, k = 0;
+	for (int k = 0; k < numsSize; k++)
+	{
+		if (nums[k] == 0)
+		{
+
+		}
+		else
+		{
+			nums[i] = nums[k];
+			i++;
+		}
+	}
+	while (i < numsSize)
+	{
+		nums[i] = 0;
+		i++;
+	}
+}
