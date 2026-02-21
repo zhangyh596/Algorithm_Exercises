c4d1260e2d50c9486105d32d2cd4d11aa8a6dde5
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-21 15:21:07 +0800

    力扣反转链表

---
+++ LeetCode/4_reverseList/4_reverseList/test.c
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+struct ListNode {
+	int val;
+	struct ListNode* next;
+};
+
+struct ListNode* reverseList(struct ListNode* head) {
+	struct ListNode* prev = NULL;
+	struct ListNode* pcur = head;
+	while (pcur)
+	{
+		struct ListNode* next = pcur->next;
+		pcur->next = prev;
+		prev = pcur;
+		pcur = next;
+	}
+	return prev;
+}
