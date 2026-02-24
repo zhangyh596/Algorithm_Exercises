8e519dbd91d22cd0a58bce4a2771397e4cd014ba
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-24 23:19:55 +0800

    力扣环形链表

---
+++ LeetCode/6_hasCycle/6_hasCycle/test.c
@@ -0,0 +1,24 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdbool.h>
+
+struct ListNode {
+	int val;
+	struct ListNode* next;
+};
+
+bool hasCycle(struct ListNode* head) {
+	struct ListNode* slow = head;
+	struct ListNode* fast = head;
+
+	while (fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+			return true;
+	}
+	return false;
+}
