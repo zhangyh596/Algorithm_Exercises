52322a40abd3b671aa51c1e0fd6011cfe44bbaf9
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-25 22:49:27 +0800

    力扣搜索插入位置

---
+++ LeetCode/8_searchInsert/8_searchInsert/test.c
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int searchInsert(int* nums, int numsSize, int target) {
+
+	if (target > nums[numsSize - 1])
+	{
+		return numsSize;
+	}
+	else if (target < nums[0])
+	{
+		return 0;
+	}
+
+	int left = 0, right = numsSize - 1;
+	while (left < right)
+	{
+		int mid = (left + right) / 2;
+		if (nums[mid] >= target)
+		{
+			right = mid;
+		}
+		else
+		{
+			left = mid + 1;
+		}
+	}
+	return left;
+}
