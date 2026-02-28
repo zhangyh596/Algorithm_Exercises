51652a35f4ebf06969c4a39396101370fe6814be
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-02-28 14:05:20 +0800

    最大子数组和

---
+++ LeetCode/11_maxSubArray/11_maxSubArray/test.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int maxSubArray(int* nums, int numsSize) {
+	int max = nums[0];
+	int s[100005];
+	s[0] = nums[0];
+	for (int i = 1; i < numsSize; i++)
+	{
+		if (s[i - 1] > 0)
+		{
+			s[i] = s[i - 1] + nums[i];
+		}
+		else
+		{
+			s[i] = nums[i];
+		}
+
+		if (s[i] > max)
+		{
+			max = s[i];
+		}
+	}
+	return max;
+}
