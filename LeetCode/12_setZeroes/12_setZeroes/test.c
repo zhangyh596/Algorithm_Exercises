c590dc64a3e4bebe2076b03a1c5f4e07995dea93
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-01 22:39:02 +0800

    矩阵置零

---
+++ LeetCode/12_setZeroes/12_setZeroes/test.c
@@ -0,0 +1,41 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+void setZeroes(int** matrix, int matrixSize, int* matrixColSize) {
+	int row_record[205] = { 0 };
+	int col_record[205] = { 0 };
+
+	for (int i = 0; i < matrixSize; i++)
+	{
+		for (int j = 0; j < *matrixColSize; j++)
+		{
+			if (matrix[i][j] == 0)
+			{
+				row_record[i] = 1;
+				col_record[j] = 1;
+			}
+		}
+	}
+
+	for (int i = 0; i < matrixSize; i++)
+	{
+		if (row_record[i] == 1)
+		{
+			for (int j = 0; j < *matrixColSize; j++)
+			{
+				matrix[i][j] = 0;
+			}
+		}
+	}
+	for (int i = 0; i < *matrixColSize; i++)
+	{
+		if (col_record[i] == 1)
+		{
+			for (int j = 0; j < matrixSize; j++)
+			{
+				matrix[j][i] = 0;
+			}
+		}
+	}
+}
