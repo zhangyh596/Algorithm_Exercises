01e57fcef0d39850440b5caa538a50351a0d5e02
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-02 23:10:28 +0800

    无重复字符的最长子串

---
+++ LeetCode/13_lengthOfLongestSubstring/13_lengthOfLongestSubstring/test.c
@@ -0,0 +1,28 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int lengthOfLongestSubstring(char* s) {
+	int note[200] = { 0 };
+	int left = 0;
+	int max_len = 0;
+	int len = strlen(s);
+	for (int right = 0; right < len; right++)
+	{
+		char c = s[right];
+
+		while (note[c] == 1)
+		{
+			note[s[left]] = 0;
+			left++;
+		}
+
+		note[c] = 1;
+
+		if (right - left + 1 > max_len)
+		{
+			max_len = right - left + 1;
+		}
+	}
+	return max_len;
+}
