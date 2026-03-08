f35a0d9074665665ce981cea7a7257c834d6b5af
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-08 10:40:51 +0800

    旋转图像

--- LeetCode/23_rotate/23_rotate/test.c
+++ LeetCode/23_rotate/23_rotate/test.c
@@ -3,5 +3,25 @@
 #include <stdio.h>
 
 void rotate(int** matrix, int matrixSize, int* matrixColSize) {
-	int new_matrix[matrixSize][*matrixColSize];
+	int** ans = (int**)malloc(matrixSize * sizeof(int*));
+	for (int i = 0; i < matrixSize; i++)
+	{
+		ans[i] = (int*)malloc(sizeof(int) * (*matrixColSize));
+	}
+
+	for (int i = 0; i < matrixSize; i++)
+	{
+		for (int j = 0; j < *matrixColSize; j++)
+		{
+			ans[j][matrixSize - i - 1] = matrix[i][j];
+		}
+	}
+	
+	for (int i = 0; i < matrixSize; i++)
+	{
+		for (int j = 0; j < *matrixColSize; j++)
+		{
+			matrix[i][j] = ans[i][j];
+		}
+	}
 }
