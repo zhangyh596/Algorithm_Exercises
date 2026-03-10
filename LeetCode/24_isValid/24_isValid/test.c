8204d616deae674d2d80d76352994a7dee091aed
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-10 08:43:52 +0800

    有效的括号

---
+++ LeetCode/24_isValid/24_isValid/test.c
@@ -0,0 +1,74 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+bool isValid(char* s) {
+	char stack[10005];
+	int top = -1;
+	int len = strlen(s);
+
+	for (int i = 0; i < len; i++)
+	{
+		char c = s[i];
+		if (c == '(' || c == '[' || c == '{')
+		{
+			top++;
+			stack[top] = c;
+		}
+		else if (c == ')')
+		{
+			if (top == -1)
+			{
+				return false;
+			}
+
+			if (stack[top] == '(')
+			{
+				top--;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else if (c == ']')
+		{
+			if (top == -1)
+			{
+				return false;
+			}
+
+			if (stack[top] == '[')
+			{
+				top--;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		else if (c == '}')
+		{
+			if (top == -1)
+			{
+				return false;
+			}
+
+			if (stack[top] == '{')
+			{
+				top--;
+			}
+			else
+			{
+				return false;
+			}
+		}
+	}
+
+	if (top != -1)
+		return false;
+	else
+		return true;
+}
