c6a12b2a8e9b61b573147579e77388ae6a1083cc
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-11 23:58:06 +0800

    搜索旋转排序数组

---
+++ LeetCode/25_search/25_search/test.c
@@ -0,0 +1,40 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int search(int* nums, int numsSize, int target) {
+	int left = 0, right = numsSize - 1;
+	while (left <= right)
+	{
+		int mid = (left + right) / 2;
+		if (nums[mid] == target)
+		{
+			return mid;
+		}
+
+		if (nums[mid] >= nums[left])
+		{
+			if (target >= nums[left] && target < nums[mid])
+			{
+				right = mid - 1;
+			}
+			else
+			{
+				left = mid + 1;
+			}
+		}
+		else
+		{
+			if (target > nums[mid] && target <= nums[right])
+			{
+				left = mid + 1;
+			}
+			else
+			{
+				right = mid - 1;
+			}
+		}
+	}
+	return -1;
+}
+
