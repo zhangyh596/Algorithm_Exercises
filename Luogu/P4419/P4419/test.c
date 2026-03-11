965ca75191e802d0d79ffa3c85a4190238272f8c
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-11 23:59:49 +0800

    P4419

---
+++ Luogu/P4419/P4419/test.c
@@ -0,0 +1,36 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int N;
+int arr[55];
+
+int main()
+{
+	int card[12] = { 0, 0, 4, 4, 4, 4, 4, 4, 4, 4, 16, 4 };
+	scanf("%d", &N);
+	int sum = 0;
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d", &arr[i]);
+		sum += arr[i];
+		card[arr[i]]--;
+	}
+
+	int X = (21 - sum) > 11 ? 11 : 21 - sum;
+	int count = 0;
+	for (int i = 2; i <= X; i++)
+	{
+		count += card[i];
+	}
+
+	if (count > 52 - N - count)
+	{
+		printf("VUCI\n");
+	}
+	else
+	{
+		printf("DOSTA\n");
+	}
+	return 0;
+}
