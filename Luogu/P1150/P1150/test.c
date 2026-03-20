440fd1d1e01b1386e747932fe2e8712e2d25f364
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-20 09:50:29 +0800

    P1150

---
+++ Luogu/P1150/P1150/test.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int main()
+{
+	int n, k;
+	scanf("%d %d", &n, &k);
+
+	int count = 0;
+	count += n;
+
+	int cigar = 0;
+	cigar += n;
+	while (cigar >= k)
+	{
+		count += cigar / k;
+		cigar = cigar % k + cigar / k;
+	}
+
+	printf("%d", count);
+	return 0;
+}
