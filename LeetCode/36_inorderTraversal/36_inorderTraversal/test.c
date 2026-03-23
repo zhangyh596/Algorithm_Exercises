ce5cfab1a948e89b5036c5d37622247208ddc528
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-23 08:52:22 +0800

    二叉树的中序遍历

---
+++ LeetCode/36_inorderTraversal/36_inorderTraversal/test.c
@@ -0,0 +1,32 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+
+void inorderHelper(struct TreeNode* root, int* ans, int* returnSize) {
+	if (root == NULL)
+	{
+		return;
+	}
+
+	inorderHelper(root->left, ans, returnSize);
+
+	ans[*returnSize] = root->val;
+	(*returnSize)++;
+
+	inorderHelper(root->right, ans, returnSize);
+}
+
+int* inorderTraversal(struct TreeNode* root, int* returnSize) {
+	int* ans = (int*)malloc(sizeof(int) * 100);
+	*returnSize = 0;
+
+	inorderHelper(root, ans, returnSize);
+	return ans;
+}
