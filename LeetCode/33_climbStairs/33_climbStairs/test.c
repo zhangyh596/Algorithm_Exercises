74e6238b3bf087f596ead8a3611915a4a89e0c5b
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-23 08:49:43 +0800

    爬楼梯

---
+++ LeetCode/33_climbStairs/33_climbStairs/test.c
@@ -0,0 +1,23 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//递归超时
+//int climbStairs(int n) {
+//	if (n == 1)
+//		return 1;
+//	else if (n == 2)
+//		return 1;
+//	else
+//		return climbStairs(n - 1) + climbStairs(n - 2);
+//}
+
+int climbStairs(int n) {
+	int dp[50];
+	dp[1] = 1, dp[2] = 2;
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+	return dp[n];
+}
