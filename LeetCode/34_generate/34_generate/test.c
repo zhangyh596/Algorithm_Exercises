4e2ed997916635241a97d593b7d62b7c003cf9c6
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-23 08:51:49 +0800

    杨辉三角

---
+++ LeetCode/34_generate/34_generate/test.c
@@ -0,0 +1,26 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
+	int** ans = (int**)malloc(sizeof(int*) * numRows);
+	*returnColumnSizes = (int*)malloc(sizeof(int) * numRows);
+	for (int i = 0; i < numRows; i++)
+	{
+		(*returnColumnSizes)[i] = i + 1;
+		ans[i] = (int*)malloc(sizeof(int) * (i + 1));
+		ans[i][0] = 1, ans[i][i] = 1;
+	}
+
+	for (int i = 0; i < numRows; i++)
+	{
+		for (int j = 1; j <= i - 1; j++)
+		{
+			ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
+		}
+	}
+
+	*returnSize = numRows;
+	return ans;
+}
