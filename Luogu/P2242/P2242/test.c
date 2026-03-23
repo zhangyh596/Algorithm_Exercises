9978d80710822af69880b10143c5897f280ab1d5
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-23 08:45:45 +0800

    P2242

---
+++ Luogu/P2242/P2242/test.c
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int n, m;
+int a[15005];
+int b[15005];
+
+int cmp(const void* p1, const void* p2)
+{
+	return *(int*)p1 - *(int*)p2;
+}
+
+int main()
+{
+	scanf("%d %d", &n, &m);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+
+		if (i > 0)
+		{
+			b[i] = a[i] - a[i - 1] - 1;
+		}
+	}
+
+	qsort(b + 1, n - 1, sizeof(int), cmp);
+	int ans = a[n - 1] - a[0] + 1;
+	for (int i = 0; i < m - 1; i++)//分成m段只需要切m - 1刀
+	{
+		ans -= b[n - 1];
+		n--;
+	}
+	printf("%d", ans);
+	return 0;
+}
