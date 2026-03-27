378e1d7ee55c7adebf9a419c714ab477e2b82b83
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-27 12:41:14 +0800

    二叉树的最大深度

---
+++ C/LeetCode/43_maxDepth/43_maxDepth/test.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+
+int maxDepth(struct TreeNode* root) {
+	if (root == NULL)
+	{
+		return 0;
+	}
+
+	int left_depth = maxDepth(root->left) + 1;
+	int right_depth = maxDepth(root->right) + 1;
+
+	if (left_depth > right_depth)
+	{
+		return left_depth;
+	}
+	else
+	{
+		return right_depth;
+	}
+}
