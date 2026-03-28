143d778da6bd2234e6604a73c0d30ab8c27328ce
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-28 14:57:55 +0800

    翻转二叉树

---
+++ C/LeetCode/44_invertTree/44_invertTree/test.c
@@ -0,0 +1,25 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+
+struct TreeNode* invertTree(struct TreeNode* root) {
+	if (root == NULL)
+	{
+		return NULL;
+	}
+
+	invertTree(root->left);
+	invertTree(root->right);
+
+	struct TreeNode* tmp = root->left;
+	root->left = root->right;
+	root->right = tmp;
+
+	return root;
+}
