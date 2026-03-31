5141cd49aee0d8385d45da36717aa3e03b0fb0c1
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-31 10:31:29 +0800

    P1152

---
+++ C/Luogu/P1152/P1152/test.c
@@ -0,0 +1,38 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int vis[1005];
+
+int main()
+{
+	int n;
+	int arr[1005];
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+		
+		if (i > 0)
+		{
+			if (abs(arr[i] - arr[i - 1]) >= 1 && abs(arr[i] - arr[i - 1]) < n)
+			{
+				vis[abs(arr[i] - arr[i - 1])] = 1;
+			}
+		}
+	}
+
+	for (int i = 1; i < n; i++)
+	{
+		if (vis[i] == 0)
+		{
+			printf("Not jolly\n");
+			return 0;
+		}
+	}
+
+	printf("Jolly\n");
+	return 0;
+}
