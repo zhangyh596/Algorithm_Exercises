2cd5c2ad82dcc8b5c52760fe2275b8e34160bb98
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-31 10:29:44 +0800

    打家劫舍

---
+++ C/LeetCode/50_rob/50_rob/test.c
@@ -0,0 +1,29 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int rob(int* nums, int numsSize) {
+	if (numsSize == 1)
+		return nums[0];
+	else if(numsSize == 2)
+		return nums[0] > nums[1] ? nums[0] : nums[1];
+
+	int dp[105];
+	dp[0] = nums[0];
+	dp[1] = nums[0] > nums[1] ? nums[0] : nums[1];
+
+	for (int i = 2; i < numsSize; i++)
+	{
+		dp[i] = dp[i - 1] > (dp[i - 2] + nums[i]) ? dp[i - 1] : (dp[i - 2] + nums[i]);
+	}
+
+	int max = dp[0];
+	for (int i = 1; i < numsSize; i++)
+	{
+		if (dp[i] > max)
+		{
+			max = dp[i];
+		}
+	}
+	return max;
+}
