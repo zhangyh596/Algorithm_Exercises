770e4bd5dfca07d9c3bf069d6f5f698cf247f98b
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-03-31 10:08:58 +0800

    将有序数组转换为二叉搜索树

---
+++ C/LeetCode/47_sortedArrayToBST/47_sortedArrayToBST/test.c
@@ -0,0 +1,60 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+
+struct TreeNode* createNode(int value)
+{
+	struct TreeNode* newNode = (struct TreeNode*)malloc(sizeof(struct TreeNode));
+	newNode->val = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+
+	return newNode;
+}
+
+struct TreeNode* insertNode(struct TreeNode* root, int value)
+{
+	if (root == NULL)
+	{
+		return createNode(value);
+	}
+
+	if (value < root->val)
+	{
+		root->left = insertNode(root->left, value);
+	}
+	else if (value > root->val)
+	{
+		root->right = insertNode(root->right, value);
+	}
+
+	return root;
+}
+
+struct TreeNode* buildTree(int* nums, int left, int right)
+{
+	if (left > right)
+	{
+		return NULL;
+	}
+
+	int mid = left + (right - left) / 2;
+
+	struct TreeNode* root = createNode(nums[mid]);
+
+	root->left = buildTree(nums, left, mid - 1);
+	root->right = buildTree(nums, mid + 1, right);
+
+	return root;
+}
+
+struct TreeNode* sortedArrayToBST(int* nums, int numsSize) {
+	return buildTree(nums, 0, numsSize - 1);
+}
