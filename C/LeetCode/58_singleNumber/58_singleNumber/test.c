bc499988d7306c3a1dbd071a8f9fb16334aa75bb
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-08 14:53:39 +0800

    只出现一次的数字

---
+++ C/LeetCode/58_singleNumber/58_singleNumber/test.c
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+//int singleNumber(int* nums, int numsSize) {
+//	int ret = 0;
+//	int count = 0;
+//	for (int i = 0; i < numsSize; i++)
+//	{
+//		int flag = 0;
+//		for (int j = 0; j < numsSize; j++)
+//		{
+//			if(nums[j] == nums[i] && j != i)
+//			{
+//				flag = 1;
+//				break;
+//			}
+//		}
+//
+//		if (flag == 0)
+//		{
+//			ret = nums[i];
+//		}
+//	}
+//	return ret;
+//}
+
+int singleNumber(int* nums, int numsSize) {
+	int ret = 0;
+	for (int i = 0; i < numsSize; i++)
+	{
+		ret ^= nums[i];
+	}
+	return ret;
+}
