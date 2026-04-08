c21389b443e5d64fca59e9c793c8e4a2ac6111c9
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-08 14:54:39 +0800

    二叉搜索树种第K小的元素

---
+++ C/LeetCode/60_kthSmallest/60_kthSmallest/test.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+
+void inorderHelper(struct TreeNode* root, int* ans, int* returnSize)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+
+	inorderHelper(root->left, ans, returnSize);
+
+	ans[*returnSize] = root->val;
+	(*returnSize)++;
+
+	inorderHelper(root->right, ans, returnSize);
+}
+
+int kthSmallest(struct TreeNode* root, int k) {
+	int* ans = (int*)malloc(sizeof(int) * 10000);
+	int size = 0;
+
+	inorderHelper(root, ans, &size);
+
+	return ans[k - 1];
+}
