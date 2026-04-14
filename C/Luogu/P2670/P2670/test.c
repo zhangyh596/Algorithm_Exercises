01f99764f615dc01165f3e93321f624ffb89c0f7
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-14 09:59:11 +0800

    P2670

---
+++ C/Luogu/P2670/P2670/test.c
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+int dx[8] = { -1, -1, -1, 0, 1, 1, 1, 0 };
+int dy[8] = { -1, 0, 1, 1, 1, 0, -1, -1 };
+
+char vis[105][105];
+
+int main()
+{
+	char map[105][105];
+	int n, m;
+	scanf("%d %d", &n, &m);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%s", map[i]);
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (map[i][j] == '*')
+			{
+				vis[i][j] = '*';
+				continue;
+			}
+
+			vis[i][j] = '0';
+			for (int k = 0; k < 8; k++)
+			{
+				int nx = i + dx[k];
+				int ny = j + dy[k];
+
+				if (nx >= 0 && nx < n && ny >= 0 && ny < m && map[nx][ny] == '*')
+				{
+					vis[i][j]++;
+				}
+			}
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("%s\n", vis[i]);
+	}
+	return 0;
+}
