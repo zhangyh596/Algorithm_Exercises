82285d2005ed640b43a89dd679b413dd6265ac72
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-18 16:59:09 +0800

    删除链表的节点

--- C/LeetCode/63_deleteNode/63_deleteNode/test.c
+++ C/LeetCode/63_deleteNode/63_deleteNode/test.c
@@ -1,1 +1,27 @@
-#define _CRT_SECURE_NO_WARNINGS 1
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+	int val;
+	struct ListNode* next;
+};
+
+struct ListNode* deleteNode(struct ListNode* head, int val) {
+	struct ListNode dummy;
+	dummy.next = head;
+
+	struct ListNode* prev = &dummy;
+	struct ListNode* pcur = dummy.next;
+	while (pcur != NULL && pcur->val != val)
+	{
+		prev = pcur;
+		pcur = pcur->next;
+	}
+
+	prev->next = pcur->next;
+	free(pcur);
+
+	return dummy.next;
+}
