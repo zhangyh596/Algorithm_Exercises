d35a84ddf3f2887866cc943144574fa283b6219f
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-20 18:03:43 +0800

    P1042

---
+++ C/Luogu/P1042/P1042/test.c
@@ -0,0 +1,74 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+char s[100000];
+
+int main()
+{
+	int len = 0;
+	char ch;
+
+	while (1)
+	{
+		scanf(" %c", &ch);
+		if (ch == 'E')
+		{
+			s[len++] = ch;
+			break;
+		}
+		s[len++] = ch;
+	}
+
+	int W = 0, L = 0;
+	for (int i = 0; i < len; i++)
+	{
+		if (s[i] == 'W')
+		{
+			W++;
+		}
+		else if (s[i] == 'L')
+		{
+			L++;
+		}
+		else if (s[i] == 'E')
+		{
+			printf("%d:%d\n", W, L);
+			break;
+		}
+
+		if ((W >= 11 || L >= 11) && abs(W - L) >= 2)
+		{
+			printf("%d:%d\n", W, L);
+			W = 0, L = 0;
+		}
+	}
+	printf("\n");
+
+	W = 0, L = 0;
+	for (int i = 0; i < len; i++)
+	{
+		if (s[i] == 'W')
+		{
+			W++;
+		}
+		else if (s[i] == 'L')
+		{
+			L++;
+		}
+		else if (s[i] == 'E')
+		{
+			printf("%d:%d\n", W, L);
+			break;
+		}
+
+		if ((W >= 21 || L >= 21) && abs(W - L) >= 2)
+		{
+			printf("%d:%d\n", W, L);
+			W = 0, L = 0;
+		}
+	}
+
+	return 0;
+}
