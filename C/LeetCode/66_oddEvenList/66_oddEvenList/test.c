dac3e820b9e262d5d079fa59cd406459f78f5447
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-04-26 11:54:07 +0800

    奇偶链表

---
+++ C/LeetCode/66_oddEvenList/66_oddEvenList/test.c
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode
+{
+	int val;
+	struct ListNode* next;
+};
+
+struct ListNode* oddEvenList(struct ListNode* head) {
+	if (head == NULL || head->next == NULL)
+	{
+		return head;
+	}
+
+	struct ListNode* odd = head;
+	struct ListNode* even = head->next;
+	struct ListNode* evenHead = even;
+
+	while (even && even->next)
+	{
+		odd->next = even->next;
+		odd = odd->next;
+
+		even->next = odd->next;
+		even = even->next;
+	}
+
+	odd->next = evenHead;
+
+	return head;
+}
