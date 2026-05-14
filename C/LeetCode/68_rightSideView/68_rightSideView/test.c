8fcff4134b0f10b2887a99918e0c35b3252f1af1
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-05-14 10:54:55 +0800

    二叉树的右视图

---
+++ C/LeetCode/68_rightSideView/68_rightSideView/test.c
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+
+#include <stdio.h>
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+
+int* rightSideView(struct TreeNode* root, int* returnSize) {
+	if (root == NULL)
+	{
+		*returnSize = 0;
+		return NULL;
+	}
+
+	struct TreeNode* queue[200];
+	int head = 0, tail = 0;
+
+	int* ans = (int*)malloc(sizeof(int) * 200);
+	*returnSize = 0;
+
+	queue[tail++] = root;
+
+	while (head < tail)
+	{
+		ans[*returnSize] = queue[tail - 1]->val;
+		(*returnSize)++;
+
+		int cur_len = tail - head;
+		for (int i = 0; i < cur_len; i++)
+		{
+			struct TreeNode* node = queue[head++];
+
+			if (node->left != NULL)
+			{
+				queue[tail++] = node->left;
+			}
+			if (node->right != NULL)
+			{
+				queue[tail++] = node->right;
+			}
+		}
+	}
+	return ans;
+}
