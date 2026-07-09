f7eca3e1ea18c4a2982809f4a31c57d280d9ae72
xiao-zhang-learns-cyy <2165911438@qq.com>
2026-07-09 12:55:16 +0800

    二叉树展开为链表

---
+++ C/LeetCode/70_flatten/70_flatten/test.cpp
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+struct TreeNode {
+	int val;
+	struct TreeNode* left;
+	struct TreeNode* right;
+};
+
+struct TreeNode* arr[2000];
+
+void preOrderTree(struct TreeNode* root, int* pcount)
+{
+	if (root == NULL)
+	{
+		return;
+	}
+
+	arr[*pcount] = root;
+	(*pcount)++;
+
+	preOrderTree(root->left, pcount);
+	preOrderTree(root->right, pcount);
+}
+
+void flatten(struct TreeNode* root)
+{
+	int count = 0;
+	preOrderTree(root, &count);
+
+	for (int i = 0; i < count; i++)
+	{
+		arr[i]->left = NULL;
+
+		if (i == count - 1)
+		{
+			arr[i]->right = NULL;
+		}
+		else
+		{
+			arr[i]->right = arr[i + 1];
+		}
+	}
+}
+
+int main()
+{
+	return 0;
+}
